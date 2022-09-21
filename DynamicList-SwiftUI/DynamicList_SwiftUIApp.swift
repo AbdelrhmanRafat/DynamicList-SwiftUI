c1962ff0428470f6c1e967f35e68630979dfdab5
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-09-21 13:42:04 +0200

    Initial Commit

---
+++ DynamicList-SwiftUI/DynamicList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicList_SwiftUIApp.swift
+//  DynamicList-SwiftUI
+//
+//  Created by Macbook on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DynamicList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
