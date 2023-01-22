2233bf28628ad75086982f36b713cc44793e8b47
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-22 11:24:24 +0200

    ‘Get_Started_List’

--- DynamicList-SwiftUI/ContentView.swift
+++ DynamicList-SwiftUI/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    //Creating Simple List View Using For Each
+        List{
+            //One thing you have to take note is that to tell For Each how to identify each of the item in the collection.. the Parameter of id is for that purpose.
+            ForEach(1...100,id : \.self) { index in
+                Text("Item  \(index)")
+            }
+        }
     }
 }
 
